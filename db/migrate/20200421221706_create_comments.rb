7f70325420156d435a67a028fbaf387e7d649ca0
github username <github email address>
2020-05-06 12:49:43 -0500

    seeded new bar images

--- db/migrate/20200421221706_create_comments.rb
+++ db/migrate/20200421221706_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[6.0]
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :bar, null: false, foreign_key: true
       t.string :comment
-      t.string :likes
 
       t.timestamps
     end
